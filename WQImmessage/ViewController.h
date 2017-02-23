74ecab78adf8b8cff6a93c5cda28bcbd2cef0755
wei qi <weiqi@hzdracom.com>
2017-02-23 11:25:49 +0800

    Initial Commit

---
+++ WQImmessage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WQImmessage
+//
+//  Created by 魏琦 on 17/2/23.
+//  Copyright © 2017年 com.drcacom.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
